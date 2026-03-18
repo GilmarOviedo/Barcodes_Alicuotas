bd96c84fc506a259af9e34c4656e806078bc0f97
GilmarOviedo <136939210+GilmarOviedo@users.noreply.github.com>
2026-03-18 15:34:52 -0500

    Update app.py

--- MUESTRAS-HUMANAS_bar_codes/app.py
+++ MUESTRAS-HUMANAS_bar_codes/app.py
@@ -6,6 +6,11 @@ import os
 import warnings
 import streamlit as st
 
+import sys
+
+
+BASE_DIR = os.path.dirname(__file__)
+sys.path.append(BASE_DIR)
 warnings.filterwarnings('ignore')
 
 from src.config import ConfiguracionCredenciales
